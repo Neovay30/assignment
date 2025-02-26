82136fefe81f3d0d5a6e16a2002b3533a3d575ae
neovay <maximevanhecke1@gmail.com>
2025-02-26 21:26:26 +0900

    feat(delete): Add dete modal and logic to BooksTable

---
+++ frontend/src/views/book/components/BookDeleteModal.tsx
@@ -0,0 +1,30 @@
+import { Modal } from "../../../components/ui/Modal";
+import Button from "../../../components/ui/Button";
+
+interface BookDeleteModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    onConfirm: () => void;
+}
+
+const BookDeleteModal = ({ isOpen, onClose, onConfirm }: BookDeleteModalProps) => {
+    return (
+        <Modal
+            isOpen={isOpen}
+            onClose={onClose}
+            title="Confirm Delete"
+        >
+            <p>Are you sure you want to delete this book?</p>
+            <div className="flex justify-end space-x-2 mt-4">
+                <Button variant="outline" onClick={onClose}>
+                    Cancel
+                </Button>
+                <Button variant="destructive" onClick={onConfirm}>
+                    Delete
+                </Button>
+            </div>
+        </Modal>
+    );
+};
+
+export default BookDeleteModal;
